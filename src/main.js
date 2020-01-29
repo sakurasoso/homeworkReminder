15b82e865a66b196981cf1a3ac825bf5537149be
Naisen Xu <723648649@qq.com>
2020-01-28 16:52:17 -0800

    test_01_28_2020_001

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ let win
 function createWindow () {
   // 创建浏览器窗口。
   win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1280,
+    height: 720,
     webPreferences: {
       nodeIntegration: true
     }
