35b2abd77ebfd2fb3e4cf0b1652f8792c9807721
Naisen Xu <723648649@qq.com>
2020-02-25 17:02:12 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, Menu } = require('electron')
 
-// 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
-// 垃圾回收的时候，window对象将会自动的关闭
+// To Check System is MAC or not
+const isMac = process.platform === 'darwin'
+
 let win
 
 function createWindow () {
-  // 创建浏览器窗口。
   win = new BrowserWindow({
     width: 1280,
     height: 720,
@@ -15,42 +15,139 @@ function createWindow () {
     title:"Homework Reminder V0.1.0",
   })
 
-  // 加载index.html文件
   win.loadFile('./html/index.html')
 
-  // 打开开发者工具
   win.webContents.openDevTools()
 
-  // 当 window 被关闭，这个事件会被触发。
   win.on('closed', () => {
-    // 取消引用 window 对象，如果你的应用支持多窗口的话，
-    // 通常会把多个 window 对象存放在一个数组里面，
-    // 与此同时，你应该删除相应的元素。
     win = null
+    if (process.platform !== 'darwin') {
+      app.quit()
+    }
   })
 }
 
-// Electron 会在初始化后并准备
-// 创建浏览器窗口时，调用这个函数。
-// 部分 API 在 ready 事件触发后才能使用。
 app.on('ready', createWindow)
 
-// 当全部窗口关闭时退出。
+
 app.on('window-all-closed', () => {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-  // 否则绝大部分应用及其菜单栏会保持激活。
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-  // 通常在应用程序中重新创建一个窗口。
   if (win === null) {
     createWindow()
   }
 })
 
-// 在这个文件中，你可以续写应用剩下主进程代码。
-// 也可以拆分成几个文件，然后用 require 导入。
+/////////// Init_Top_Menu //////////////////
+const top_Menu_Data = [
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  {
+    label: 'File',
+    submenu: [
+      { label: 'import from file' },
+    ]
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startspeaking' },
+            { role: 'stopspeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' }
+    ]
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [
+      { role: 'minimize' },
+      { role: 'zoom' },
+      ...(isMac ? [
+        { type: 'separator' },
+        { role: 'front' },
+        { type: 'separator' },
+        { role: 'window' }
+      ] : [
+        { role: 'close' }
+      ])
+    ]
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          const { shell } = require('electron')
+          await shell.openExternal('https://github.com/sakurasoso/homeworkReminder')
+        }
+      }
+    ]
+  },
+  {
+    label: 'DevKit',
+    submenu: [
+      {
+        label: 'Show Test Reminder',
+        click: async() =>{
+          const {Notification} = require('electron');
+          const test_Noti  = new Notification({
+            title: 'test',
+          });
+          test_Noti.show();
+        }
+      },
+    ]
+  },
+]
+
+const top_Menu = Menu.buildFromTemplate(top_Menu_Data)
+Menu.setApplicationMenu(top_Menu)
